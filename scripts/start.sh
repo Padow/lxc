de86c793e9f5111667551d10d77db5006148dd9b
Patrick DOUCHEMENT <patrick.douchement@fastconnect.fr>
2017-06-28 10:30:34 +0200

    add retry to get IP

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,24 @@ for (( i = 0; i < $MAX; i++ )); do
 	sudo lxc-create -n centos_$i -t /usr/share/lxc/templates/lxc-centos
 	echo "root"| sudo chroot /var/lib/lxc/centos_$i/rootfs passwd root --stdin
 	sudo lxc-start -n centos_$i -d
-	IP=`sudo lxc-info -n centos_$i|grep IP: | cut -d':' -f2 |sed -e 's/^[ \t]*//'`
-  export IP_LIST=$IP_LIST" "$IP
-	echo $IP >> ~/output.log
+done
+
+RETRY(){
+	TRIES=0
+	IP=""
+	while [ "$TRIES" -lt  "20" ];   do
+    TRIES=$((TRIES+1))
+    IP=`sudo lxc-info -n centos_$1 -iH`
+    if [ "$IP" == "" ]; then
+      echo "IP is empty"
+    else
+	    export IP_LIST=$IP_LIST" "$IP
+	    TRIES=20
+    fi
+    sleep 2
+	done
+}
+
+for (( i = 0; i < $MAX; i++ )); do
+  RETRY $i
 done
