b0849ff79f93ef3e5d72abcea3f69685cca1dd0c
Patrick DOUCHEMENT <patrick.douchement@fastconnect.fr>
2017-06-30 18:13:24 +0200

    rework for ubuntu images

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-sudo yum install -y epel-release
-sudo yum install -y debootstrap perl libvirt
-sudo yum install -y lxc lxc-templates
-sudo yum install -y /usr/bin/lxc-ls
-
-sudo systemctl start lxc.service
-sudo systemctl enable lxc.service
-sudo systemctl start libvirtd 
-sudo systemctl enable libvirtd 
-sudo systemctl status lxc.service
-
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y lxc lxctl lxc-templates
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,28 +1,47 @@
 #!/bin/bash
 
-export IP_LIST=""
+ARRAY=()
 for (( i = 0; i < $MAX; i++ )); do
-	sudo lxc-create -n centos_$i -t /usr/share/lxc/templates/lxc-centos
-	echo "root"| sudo chroot /var/lib/lxc/centos_$i/rootfs passwd root --stdin
-	sudo lxc-start -n centos_$i -d
+    lxc-create -n ubuntu_$i -t /usr/share/lxc/templates/lxc-ubuntu
+    lxc-start -n ubuntu_$i -d
 done
 
-RETRY(){
-	TRIES=0
-	IP=""
-	while [ "$TRIES" -lt  "20" ];   do
-    TRIES=$((TRIES+1))
-    IP=`sudo lxc-info -n centos_$1 -iH`
-    if [ "$IP" == "" ]; then
-      echo "IP is empty"
-    else
-	    export IP_LIST=$IP_LIST" "$IP
-	    TRIES=20
-    fi
-    sleep 2
-	done
+ADD_IP_TABLE(){
+  iptables -t nat -A PREROUTING -i eth0 -p tcp --dport $2 -j DNAT --to-destination $1:22
 }
 
+ADD_TO_HOSTPOOL(){
+  JSON='{"default": {"os": "linux", "endpoint": {"port": '$1', "protocol": "ssh"}}, "hosts":[{"name": "ubuntu_'$i'", "endpoint":{"ip": "'$2'"},"credentials":{"username":"ubuntu","key":"ubuntu"}}]}'
+  curl -v -X POST -H "Content-Type: application/json" -d $JSON  $3
+}
+
+RETRY(){
+    TRIES=0
+    IP=""
+    MAP_PORT=$2
+    while [ "$TRIES" -lt  "20" ];   do
+    TRIES=$((TRIES+1))
+    IP=`sudo lxc-info -n ubuntu_$1 -iH`
+    if [ "$IP" == "" ]; then
+        echo "No IP sets rety "$TRIES
+    else
+        MAPPING=$HOST_PRIVATE_IP:$2
+        ARRAY+=($MAPPING)
+        ADD_IP_TABLE $IP $MAP_PORT
+        ADD_TO_HOSTPOOL $MAP_PORT $HOST_PRIVATE_IP $HOSTPOOL_ENDPOINT
+        TRIES=20
+    fi
+    sleep 2
+    done
+}
+
+BASE_PORT=8022
 for (( i = 0; i < $MAX; i++ )); do
-  RETRY $i
+  PORT=$(($BASE_PORT + $i))
+  RETRY $i $PORT
 done
+
+for i in "${ARRAY[@]}"; do
+	STR=$i", "$STR
+done
+IP_LIST=${STR::-2}
